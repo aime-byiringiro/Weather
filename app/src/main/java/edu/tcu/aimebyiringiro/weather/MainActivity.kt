93186fb9e7fd7ebc795bd5bf6d584569a6de3e46
aimebyiringiro <byiringiroaimeismael@gmail.com>
2024-11-14 14:42:31 -0600

    done adding binding

--- app/src/main/java/edu/tcu/aimebyiringiro/weather/MainActivity.kt
+++ app/src/main/java/edu/tcu/aimebyiringiro/weather/MainActivity.kt
@@ -22,18 +22,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         view = binding.root // or view = binding.main
-
-
-
-
-
-
-
-        setContentView(R.layout.activity_main)
+        setContentView(view)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
