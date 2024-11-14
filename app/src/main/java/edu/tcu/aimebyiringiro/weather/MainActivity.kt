cb865c00fa71d40af6607fb50bb3234a9ebf5fe1
aimebyiringiro <byiringiroaimeismael@gmail.com>
2024-11-14 14:40:50 -0600

    init commit

---
+++ app/src/main/java/edu/tcu/aimebyiringiro/weather/MainActivity.kt
@@ -0,0 +1,43 @@
+package edu.tcu.aimebyiringiro.weather
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import edu.tcu.aimebyiringiro.weather.databinding.ActivityMainBinding
+
+/*
+Update location
+Launch google maps
+Clear the cache
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var view: View
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        view = binding.root // or view = binding.main
+
+
+
+
+
+
+
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
