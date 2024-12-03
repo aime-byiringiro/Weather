a7e3099bffbb7b1531bcae003927fd15502bd4af
aimebyiringiro <byiringiroaimeismael@gmail.com>
2024-12-03 11:13:04 -0600

    initial commit

--- app/src/main/java/edu/tcu/aimebyiringiro/weather/MainActivity.kt
+++ app/src/main/java/edu/tcu/aimebyiringiro/weather/MainActivity.kt
@@ -21,6 +21,7 @@ import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
 import edu.tcu.aimebyiringiro.weather.model.WeatherResponse
 import edu.tcu.aimebyiringiro.weather.databinding.ActivityMainBinding
+import edu.tcu.aimebyiringiro.weather.m.Place
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -45,6 +46,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var view: View
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var weatherService: WeatherService
+    private lateinit var geoService: GeoService
     private lateinit var weatherResponse: WeatherResponse
     // Register the permissions callback, which handles the user's response to the
 // system permissions dialog. Save the return value, an instance of
@@ -57,12 +59,16 @@ class MainActivity : AppCompatActivity() {
             if (isGranted) {
                 updateLocationAndWeatherRepeatedly()
             } else {
+
             }
         }
 
     private var cancellationTokenSource: CancellationTokenSource? = null
 
     private var weatherServiceCall: Call<WeatherResponse>? = null
+
+    private var geoServiceCall: Call<List<Place>>? = null
+    private var geoResponse: List<Place>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -79,9 +85,10 @@ class MainActivity : AppCompatActivity() {
         val retrofit = Retrofit.Builder()
             .baseUrl(getString(R.string.base_url))
             .addConverterFactory(GsonConverterFactory.create())
-            .build();
+            .build()
 
         weatherService = retrofit.create(WeatherService::class.java)
+        geoService = retrofit.create(GeoService::class.java)
 
         requestLocationPermission()
     }
@@ -179,13 +186,10 @@ class MainActivity : AppCompatActivity() {
                         updatePlace(location)
                         displayWeather()
                     }
-
-                    TODO("Not yet implemented")
                 }
 
                 override fun onFailure(p0: Call<WeatherResponse>, p1: Throwable) {
                     displayUpdateFailed()
-                    TODO("Not yet implemented")
                 }
 
             }
@@ -216,16 +220,57 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updatePlace(location: Location) {
-        TODO("Not yet implemented")
+        geoServiceCall = geoService.getPlace(
+            lat = location.latitude,
+            lon = location.longitude,
+            appid = getString(R.string.api_key)
+        )
+        geoServiceCall?.enqueue(object : Callback<List<Place>> {
+            override fun onResponse(call: Call<List<Place>>, response: Response<List<Place>>) {
+                if (response.isSuccessful) {
+                    val responseList = response.body()
+                    if (!responseList.isNullOrEmpty()) {
+                        geoResponse = responseList // Save the result for use in displayPlace()
+                        displayPlace()
+                    } else {
+                        displayUpdateFailed()
+                    }
+                } else {
+                    displayUpdateFailed()
+                }
+            }
+
+            override fun onFailure(call: Call<List<Place>>, t: Throwable) {
+                displayUpdateFailed()
+            }
+        })
     }
 
+
+
     private fun displayUpdateFailed() {
-        TODO("Not yet implemented")
+
     }
 
     private fun displayPlace() {
-       binding.placeTv.text =
-           getString(R.string.place, geoResponse[0].name, geoResponse[0].state)
+        geoResponse?.let { places ->
+            val place = places[0] // Always use the first place from the response
+
+            // Format the location string based on availability of state
+            val locationName = if (place.state.isNullOrEmpty()) {
+                getString(R.string.place, place.name, place.country) // Use city and country
+            } else {
+                getString(R.string.place, place.name, place.state) // Use city and state
+            }
+
+            // Set the formatted string to the TextView
+            binding.placeTv.text = locationName
+        } ?: run {
+            // If geoResponse is null, show a fallback message
+            displayUpdateFailed()
+        }
     }
+
+
 
 }
